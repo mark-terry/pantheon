23ce4415b9ed469593f0c2cc742871db77bfaa5f
Mark Terry <mark.terry@consensys.net>
2019-01-31 15:42:17 +1000

    [NC-1968] WhitelistPersistorTest unit tests.

--- ethereum/permissioning/src/test/java/tech/pegasys/pantheon/ethereum/permissioning/WhitelistPersistorTest.java
+++ ethereum/permissioning/src/test/java/tech/pegasys/pantheon/ethereum/permissioning/WhitelistPersistorTest.java
@@ -12,6 +12,10 @@
  */
 package tech.pegasys.pantheon.ethereum.permissioning;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import tech.pegasys.pantheon.ethereum.permissioning.WhitelistPersistor.WHITELIST_TYPE;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,6 +23,7 @@ import java.nio.file.StandardOpenOption;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import com.google.common.collect.Lists;
 import org.junit.After;
@@ -30,9 +35,9 @@ public class WhitelistPersistorTest {
   private WhitelistPersistor whitelistPersistor;
   private File tempFile;
   private final String accountsWhitelist =
-      String.format("%s=%s", WhitelistPersistor.WHITELIST_TYPE.ACCOUNTS.getTomlKey(), "meow,meow");
+      String.format("%s=[%s]", WHITELIST_TYPE.ACCOUNTS.getTomlKey(), "\"account1\",\"account2\"");
   private final String nodesWhitelist =
-      String.format("%s=%s", WhitelistPersistor.WHITELIST_TYPE.NODES.getTomlKey(), "woof,woof");
+      String.format("%s=[%s]", WHITELIST_TYPE.NODES.getTomlKey(), "\"node1\",\"node2\"");
 
   @Before
   public void setUp() throws IOException {
@@ -44,24 +49,87 @@ public class WhitelistPersistorTest {
 
   @Test
   public void lineShouldBeRemoved() throws IOException {
-    whitelistPersistor.removeExistingConfigItem(WhitelistPersistor.WHITELIST_TYPE.ACCOUNTS);
+    WHITELIST_TYPE keyForRemoval = WHITELIST_TYPE.ACCOUNTS;
+
+    assertThat(countLines()).isEqualTo(2);
+    assertThat(hasKey(keyForRemoval)).isTrue();
+
+    whitelistPersistor.removeExistingConfigItem(keyForRemoval);
+
+    assertThat(countLines()).isEqualTo(1);
+    assertThat(hasKey(keyForRemoval)).isFalse();
   }
 
   @Test
   public void lineShouldBeAdded() throws IOException {
-    Set<String> updatedWhitelist = Collections.singleton("moo");
-    whitelistPersistor.removeExistingConfigItem(WhitelistPersistor.WHITELIST_TYPE.NODES);
-    whitelistPersistor.addNewConfigItem(WhitelistPersistor.WHITELIST_TYPE.NODES, updatedWhitelist);
+    final WHITELIST_TYPE key = WHITELIST_TYPE.NODES;
+    final Set<String> updatedWhitelist = Collections.singleton("node5");
+
+    assertThat(countLines()).isEqualTo(2);
+    assertThat(hasKey(key)).isTrue();
+
+    whitelistPersistor.removeExistingConfigItem(WHITELIST_TYPE.NODES);
+
+    assertThat(countLines()).isEqualTo(1);
+    assertThat(hasKey(key)).isFalse();
+
+    whitelistPersistor.addNewConfigItem(key, updatedWhitelist);
+
+    assertThat(countLines()).isEqualTo(2);
+    assertThat(hasKey(key)).isTrue();
   }
 
   @Test
   public void lineShouldBeReplaced() throws IOException {
-    Set<String> updatedWhitelist = Collections.singleton("moo");
-    whitelistPersistor.updateConfig(WhitelistPersistor.WHITELIST_TYPE.NODES, updatedWhitelist);
+    WHITELIST_TYPE key = WHITELIST_TYPE.NODES;
+    String newValue = "node5";
+
+    assertThat(countLines()).isEqualTo(2);
+    assertThat(hasKeyAndExactLineContent(key, nodesWhitelist)).isTrue();
+
+    whitelistPersistor.updateConfig(key, Collections.singleton(newValue));
+
+    assertThat(countLines()).isEqualTo(2);
+    assertThat(hasKeyAndContainsValue(key, newValue)).isTrue();
+    assertThat(hasKeyAndExactLineContent(key, nodesWhitelist)).isFalse();
+  }
+
+  @Test
+  public void outputIsValidTOML() throws IOException {
+    WHITELIST_TYPE key = WHITELIST_TYPE.ACCOUNTS;
+    List<String> newValue = Lists.newArrayList("account5", "account6", "account4");
+    String expectedValue =
+        String.format("%s=[%s]", "accounts-whitelist", "\"account5\",\"account6\",\"account4\"");
+
+    whitelistPersistor.updateConfig(key, newValue);
+
+    assertThat(hasKey(key)).isTrue();
+    assertThat(hasKeyAndExactLineContent(key, expectedValue)).isTrue();
   }
 
   @After
   public void tearDown() {
     tempFile.delete();
+  }
+
+  private long countLines() throws IOException {
+    return Files.lines(tempFile.toPath()).count();
+  }
+
+  private boolean hasKey(final WHITELIST_TYPE key) throws IOException {
+    Stream<String> lines = Files.lines(tempFile.toPath());
+    return lines.anyMatch(s -> s.startsWith(key.getTomlKey()));
+  }
+
+  private boolean hasKeyAndContainsValue(final WHITELIST_TYPE key, final String value)
+      throws IOException {
+    Stream<String> lines = Files.lines(tempFile.toPath());
+    return lines.anyMatch(s -> s.startsWith(key.getTomlKey()) && s.contains(value));
+  }
+
+  private boolean hasKeyAndExactLineContent(final WHITELIST_TYPE key, final String exactKeyValue)
+      throws IOException {
+    Stream<String> lines = Files.lines(tempFile.toPath());
+    return lines.anyMatch(s -> s.startsWith(key.getTomlKey()) && s.equals(exactKeyValue));
   }
 }
